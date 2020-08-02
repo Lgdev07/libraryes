dbf2b027a774fc99ff615231760f9b862f7abe5e
Lgdev07 <luan_saopaulo06@hotmail.com>
2020-08-01 23:44:56 -0300

    [NEW] Add new methods in bookController.
    - Update book and Delete book.

--- controllers/bookController.go
+++ controllers/bookController.go
@@ -3,10 +3,12 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/Lgdev07/olist_challenge/models"
 	"github.com/Lgdev07/olist_challenge/utils"
+	"github.com/gorilla/mux"
 )
 
 type Response struct {
@@ -27,9 +29,7 @@ func (s *Server) createBook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	authors := &models.Author{}
-
-	authorsList, _ := authors.GetAuthorsById(s.DB, response.Authors)
+	authorsList, _ := models.GetAuthorsById(s.DB, response.Authors)
 
 	book := &models.Book{
 		Name:            response.Name,
@@ -48,6 +48,64 @@ func (s *Server) createBook(w http.ResponseWriter, r *http.Request) {
 	utils.JSON(w, http.StatusCreated, resp)
 	return
 
+}
+
+func (s *Server) DeleteBook(w http.ResponseWriter, r *http.Request) {
+	var resp = map[string]interface{}{"status": "success"}
+
+	params := mux.Vars(r)
+
+	intID, _ := strconv.Atoi(params["id"])
+
+	book, _ := models.ShowBook(s.DB, intID)
+	if book.ID == 0 {
+		resp["status"] = "failed"
+		resp["message"] = "Book not Found"
+	}
+
+	if err := models.DeleteBook(s.DB, intID); err != nil {
+		utils.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	resp["message"] = "Deleted Book " + book.Name
+	utils.JSON(w, http.StatusOK, resp)
+	return
+}
+
+func (s *Server) UpdateBook(w http.ResponseWriter, r *http.Request) {
+	var resp = map[string]interface{}{"status": "success", "message": "Book Updated"}
+
+	params := mux.Vars(r)
+
+	intID, _ := strconv.Atoi(params["id"])
+
+	response := &Response{}
+
+	err := json.NewDecoder(r.Body).Decode(response)
+	if err != nil {
+		utils.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	authorsList, _ := models.GetAuthorsById(s.DB, response.Authors)
+
+	book := &models.Book{
+		Name:            response.Name,
+		Edition:         response.Edition,
+		PublicationYear: response.PublicationYear,
+		Authors:         *authorsList,
+	}
+
+	updatedBook, err := book.UpdateBook(s.DB, intID)
+	if err != nil {
+		utils.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	resp["book"] = updatedBook
+	utils.JSON(w, http.StatusOK, resp)
+	return
 }
 
 func (s *Server) ListBooks(w http.ResponseWriter, r *http.Request) {
