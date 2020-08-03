71913bb0b92ff55d881e98c00dbc5b40ab92a96c
Lgdev07 <luan_saopaulo06@hotmail.com>
2020-08-02 23:34:31 -0300

    [NEW] Add function to get all books with params

--- models/books.go
+++ models/books.go
@@ -21,20 +21,41 @@ func (b *Book) Save(db *gorm.DB) (*Book, error) {
 
 }
 
-func (b *Book) GetAllBooks(db *gorm.DB) (*[]Book, error) {
-	books := []Book{}
+func GetAllBooks(db *gorm.DB, params map[string]string) (*[]Book, error) {
+	books := &[]Book{}
 
-	err := db.Debug().Table("books").Find(&books).Error
+	chain := db.Debug().Preload("Authors")
+	chain = chain.Joins("inner join book_authors on book_authors.book_id = books.id")
+	chain = chain.Joins("inner join authors on authors.id = book_authors.author_id")
+	chain = chain.Where("")
+
+	if params["name"] != "" {
+		chain = chain.Where("books.name = ?", params["name"])
+	}
+
+	if params["publicationYear"] != "" {
+		chain = chain.Where("publication_year = ?", params["publicationYear"])
+	}
+
+	if params["edition"] != "" {
+		chain = chain.Where("edition = ?", params["edition"])
+	}
+
+	if params["author"] != "" {
+		chain = chain.Where("authors.name = ?", params["author"])
+	}
+
+	err := chain.Group("books.id").Find(books).Error
 	if err != nil {
 		return &[]Book{}, err
 	}
-	return &books, nil
+	return books, nil
 }
 
 func ShowBook(db *gorm.DB, id int) (*Book, error) {
 	book := &Book{}
 
-	err := db.Debug().Table("books").Where("id = ?", id).First(book).Error
+	err := db.Debug().Preload("Authors").Table("books").Where("id = ?", id).First(book).Error
 	if err != nil {
 		return &Book{}, err
 	}
